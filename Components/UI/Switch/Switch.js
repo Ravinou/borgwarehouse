46c41f9b660ab68793bce1be39debe43738881d7
bsourisse <dev@r4ven.fr>
2023-01-16 18:27:27 +0100

    ui: create a switch components for settings

---
+++ Components/UI/Switch/Switch.js
@@ -0,0 +1,24 @@
+//Lib
+import classes from './Switch.module.css';
+
+export default function Switch(props) {
+    return (
+        <>
+            <div className={classes.switchWrapper}>
+                <div className={classes.switch}>
+                    <label className={classes.pureMaterialSwitch}>
+                        <input
+                            type='checkbox'
+                            onChange={(e) => props.onChange(e.target.checked)}
+                        />
+
+                        <span>{props.switchName}</span>
+                    </label>
+                </div>
+                <div className={classes.switchDescription}>
+                    <span>{props.switchDescription}</span>
+                </div>
+            </div>
+        </>
+    );
+}

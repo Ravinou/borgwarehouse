053626c75775c89e563e1fbcf84e62d83e69555f
bsourisse <dev@r4ven.fr>
2023-01-18 14:34:14 +0100

    feat: API to get emailAlert status for useraccount

---
+++ pages/api/account/getEmailAlert.js
@@ -0,0 +1,63 @@
+//Lib
+import { promises as fs } from 'fs';
+import path from 'path';
+import { authOptions } from '../../../pages/api/auth/[...nextauth]';
+import { unstable_getServerSession } from 'next-auth/next';
+
+export default async function handler(req, res) {
+    if (req.method == 'GET') {
+        //Verify that the user is logged in.
+        const session = await unstable_getServerSession(req, res, authOptions);
+        if (!session) {
+            res.status(401).json({ message: 'You must be logged in.' });
+            return;
+        }
+        try {
+            //Read the users file
+            //Find the absolute path of the json directory
+            const jsonDirectory = path.join(process.cwd(), '/config');
+            let usersList = await fs.readFile(
+                jsonDirectory + '/users.json',
+                'utf8'
+            );
+            //Parse the usersList
+            usersList = JSON.parse(usersList);
+
+            //Verify that the user of the session exists
+            const userIndex = usersList
+                .map((user) => user.username)
+                .indexOf(session.user.name);
+            if (userIndex === -1) {
+                res.status(400).json({
+                    message:
+                        'User is incorrect. Please, logout to update your session.',
+                });
+                return;
+            } else {
+                //Send the emailAlert bool
+                res.status(200).json({
+                    emailAlert: usersList[userIndex].emailAlert,
+                });
+                return;
+            }
+        } catch (error) {
+            //Log for backend
+            console.log(error);
+            //Log for frontend
+            if (error.code == 'ENOENT') {
+                res.status(500).json({
+                    status: 500,
+                    message: 'No such file or directory',
+                });
+            } else {
+                res.status(500).json({
+                    status: 500,
+                    message: 'API error, contact the administrator',
+                });
+            }
+            return;
+        }
+    } else {
+        res.status(405).json({ message: 'Bad request on API' });
+    }
+}

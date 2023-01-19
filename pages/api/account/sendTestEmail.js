45093e0ee61d8a3b1a1a605b3439d6481a586981
bsourisse <dev@r4ven.fr>
2023-01-19 18:14:16 +0100

    feat: add option to allow self-signed TLS SMTP

--- pages/api/account/sendTestEmail.js
+++ pages/api/account/sendTestEmail.js
@@ -20,7 +20,11 @@ export default async function handler(req, res) {
                 user: process.env.MAIL_SMTP_LOGIN,
                 pass: process.env.MAIL_SMTP_PWD,
             },
-            secure: process.env.MAIL_SMTP_SECURE,
+            secure: process.env.MAIL_SMTP_SECURE, //Use TLS or not
+            tls: {
+                // do not fail on invalid certs >> allow self-signed or invalid TLS certificate
+                rejectUnauthorized: process.env.MAIL_REJECT_SELFSIGNED_TLS,
+            },
         });
         //Mail options
         const mailData = {

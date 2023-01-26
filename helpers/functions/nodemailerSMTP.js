52f84bdfebc1eca3ce01e3839100d35df984a654
bsourisse <dev@r4ven.fr>
2023-01-26 11:43:38 +0100

    fix: TLS error with port 587 ssl3:wrong ver number

--- helpers/functions/nodemailerSMTP.js
+++ helpers/functions/nodemailerSMTP.js
@@ -9,7 +9,6 @@ export default function nodemailerSMTP() {
             user: process.env.MAIL_SMTP_LOGIN,
             pass: process.env.MAIL_SMTP_PWD,
         },
-        secure: process.env.MAIL_SMTP_SECURE, //Use TLS or not
         tls: {
             // do not fail on invalid certs >> allow self-signed or invalid TLS certificate
             rejectUnauthorized: process.env.MAIL_REJECT_SELFSIGNED_TLS,

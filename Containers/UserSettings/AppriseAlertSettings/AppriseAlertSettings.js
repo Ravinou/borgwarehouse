20ccb274666c34a10c03bc135f70904d1698538c
bsourisse <dev@r4ven.fr>
2023-03-03 16:11:38 +0100

    feat: apprise notification switch + api

--- Containers/UserSettings/AppriseAlertSettings/AppriseAlertSettings.js
+++ Containers/UserSettings/AppriseAlertSettings/AppriseAlertSettings.js
@@ -11,6 +11,79 @@ import Error from '../../../Components/UI/Error/Error';
 import Switch from '../../../Components/UI/Switch/Switch';
 
 export default function AppriseAlertSettings() {
+    //Var
+    const toastOptions = {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        //Callback > re-enabled button after notification.
+        onClose: () => setDisabled(false),
+    };
+
+    ////State
+    const [isLoading, setIsLoading] = useState(true);
+    const [testIsLoading, setTestIsLoading] = useState(false);
+    const [error, setError] = useState();
+    const [disabled, setDisabled] = useState(false);
+    const [checked, setChecked] = useState();
+    const [info, setInfo] = useState(false);
+
+    ////LifeCycle
+    //Component did mount
+    useEffect(() => {
+        const dataFetch = async () => {
+            try {
+                const response = await fetch('/api/account/getAppriseAlert', {
+                    method: 'GET',
+                    headers: {
+                        'Content-type': 'application/json',
+                    },
+                });
+                setChecked((await response.json()).appriseAlert);
+                setIsLoading(false);
+            } catch (error) {
+                console.log('Fetching Apprise alert setting failed.');
+            }
+        };
+        dataFetch();
+    }, []);
+
+    ////Functions
+    const onChangeSwitchHandler = async (data) => {
+        //Remove old error
+        setError();
+        //Disabled button
+        setDisabled(true);
+        const response = await fetch('/api/account/updateAppriseAlert', {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        const result = await response.json();
+
+        if (!response.ok) {
+            setError(result.message);
+            setTimeout(() => {
+                setError();
+                setDisabled(false);
+            }, 4000);
+        } else {
+            if (data.appriseAlert) {
+                setChecked(!checked);
+                toast.success('Apprise notifications enabled.', toastOptions);
+            } else {
+                setChecked(!checked);
+                toast.success('Apprise notifications disabled.', toastOptions);
+            }
+        }
+    };
+
     return (
         <>
             {/* APPRISE ALERT */}
@@ -20,15 +93,26 @@ export default function AppriseAlertSettings() {
                 </div>
                 <div className={classes.setting}>
                     <div className={classes.bwFormWrapper}>
-                        <Switch
-                            // checked={checked}
-                            // disabled={disabled}
-                            switchName='Notify my Apprise services'
-                            switchDescription='You will receive an alert on all your services every 24H if you have a down status.'
-                            // onChange={(e) =>
-                            //     onChangeSwitchHandler({ emailAlert: e })
-                            // }
-                        />
+                        {isLoading ? (
+                            <SpinnerCircularFixed
+                                size={30}
+                                thickness={150}
+                                speed={150}
+                                color='#704dff'
+                                secondaryColor='#c3b6fa'
+                            />
+                        ) : (
+                            <Switch
+                                checked={checked}
+                                disabled={disabled}
+                                switchName='Notify my Apprise services'
+                                switchDescription='You will receive an alert on all your services every 24H if you have a down status.'
+                                onChange={(e) =>
+                                    onChangeSwitchHandler({ appriseAlert: e })
+                                }
+                            />
+                        )}
+
                         <form
                             className={
                                 classes.bwForm + ' ' + classes.currentSetting
@@ -43,7 +127,7 @@ export default function AppriseAlertSettings() {
                             >
                                 Apprise URLs
                             </div>
-                            <textarea></textarea>
+                            <textarea style={{ height: '100px' }}></textarea>
                         </form>
                         <div
                             style={{
@@ -70,6 +154,7 @@ export default function AppriseAlertSettings() {
                         >
                             Send a test notification
                         </button>
+                        {error && <Error message={error} />}
                     </div>
                 </div>
             </div>

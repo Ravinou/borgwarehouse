2e94f37fe4a1d94840602b359b3841a92eecc3bf
York-Simon Johannsen <info@yosijo.de>
2023-06-08 12:44:53 +0200

    Fix: SC2086 on deleteRepo.sh

--- helpers/shells/deleteRepo.sh
+++ helpers/shells/deleteRepo.sh
@@ -13,7 +13,7 @@ if [[ $# -ne 1 || $1 = "" ]]; then
     exit 1
 fi
 
-# Check if username length is 8 char. With createRepo.sh our randoms have a length of 8 characters. 
+# Check if username length is 8 char. With createRepo.sh our randoms have a length of 8 characters.
 # If we receive another length there is necessarily a problem.
 username=$1
 if [ ${#username} != 8 ]
@@ -24,7 +24,7 @@ fi
 
 # Delete the user if it exists
 if id "$1" &>/dev/null; then
-    sudo userdel -rf $1
+    sudo userdel -rf "$1"
     echo "The user $1 and all his data have been deleted"
 else
     echo "The user $1 does not exist"

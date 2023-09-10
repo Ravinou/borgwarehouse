ddc52291365eb9047c3179b831ac3690e62ee8c6
Ravinou <dev@r4ven.fr>
2023-09-10 15:58:08 +0200

    style: all files now match prettier conf

--- Components/UI/Layout/Header/Header.js
+++ Components/UI/Layout/Header/Header.js
@@ -1,23 +1,21 @@
 //Lib
-import classes from "./Header.module.css";
+import classes from './Header.module.css';
 
 //Components
-import Nav from "./Nav/Nav";
+import Nav from './Nav/Nav';
 
 function Header() {
     return (
         <header className={classes.Header}>
             <div className={[classes.flex, 'container'].join(' ')}>
-                <div className={classes.logo}>
-                    BorgWarehouse
-                </div>
+                <div className={classes.logo}>BorgWarehouse</div>
 
                 <nav>
                     <Nav />
-                </nav> 
+                </nav>
             </div>
         </header>
-    )
+    );
 }
 
 export default Header;

6d32861ce65a90216a5e1b641d1a3adc3c1b0a1b
Ravinou <dev@r4ven.fr>
2023-11-15 10:50:27 +0100

    fix: init alert settings with account

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -26,6 +26,8 @@ export const authOptions = {
                                 password:
                                     '$2a$12$20yqRnuaDBH6AE0EvIUcEOzqkuBtn1wDzJdw2Beg8w9S.vEqdso0a',
                                 roles: ['admin'],
+                                emailAlert: false,
+                                appriseAlert: false,
                             },
                         ])
                     );

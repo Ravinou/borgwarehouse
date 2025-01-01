c5b2e0b57575dfc2f093c17285870ea462e7f5a2
Ravinou <dev@r4ven.fr>
2025-01-01 12:38:16 +0100

    refactor: ⚡ components Repo and auth config

---
+++ domain/config.types.ts
@@ -0,0 +1,39 @@
+export type Repository = {
+  id: number;
+  alias: string;
+  repositoryName: string;
+  status: boolean;
+  lastSave: number;
+  alert: number;
+  storageSize: number;
+  storageUsed: number;
+  sshPublicKey: string;
+  comment: string;
+  displayDetails: boolean;
+  unixUser: string;
+  lanCommand: boolean;
+  appendOnlyMode: boolean;
+};
+
+export type BorgWarehouseUser = {
+  id: number;
+  username: string;
+  password: string;
+  roles: string[];
+  email: string;
+  emailAlert?: boolean;
+  appriseAlert?: boolean;
+};
+
+export enum WizardEnv {
+  UNIX_USER = 'UNIX_USER',
+  FQDN = 'FQDN',
+  SSH_SERVER_PORT = 'SSH_SERVER_PORT',
+  FQDN_LAN = 'FQDN_LAN',
+  SSH_SERVER_PORT_LAN = 'SSH_SERVER',
+  SSH_SERVER_FINGERPRINT_RSA = 'SSH_SERVER_FINGERPRINT_RSA',
+  SSH_SERVER_FINGERPRINT_ECDSA = 'SSH_SERVER_FINGERPRINT_ECDSA',
+  SSH_SERVER_FINGERPRINT_ED25519 = 'SSH_SERVER_FINGERPRINT_ED25519',
+  HIDE_SSH_PORT = 'HIDE_SSH_PORT',
+  DISABLE_INTEGRATIONS = 'DISABLE_INTEGRATIONS',
+}

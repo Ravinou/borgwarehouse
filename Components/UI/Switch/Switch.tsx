940367e6b205e73ca433d99abd6ed99da58e00d2
Ravinou <dev@r4ven.fr>
2025-01-26 13:37:13 +0100

    refactor: ⚡ apprise alert and switch component

--- Components/UI/Switch/Switch.tsx
+++ Components/UI/Switch/Switch.tsx
@@ -1,35 +1,45 @@
 //Lib
 import { Optional } from '~/types';
 import classes from './Switch.module.css';
+import { SpinnerCircularFixed } from 'spinners-react';
 
 type SwitchProps = {
   switchName: string;
   switchDescription: string;
   checked: Optional<boolean>;
   disabled: boolean;
+  loading?: boolean;
   onChange: (checked: boolean) => void;
 };
 
 export default function Switch(props: SwitchProps) {
   return (
-    <>
-      <div className={classes.switchWrapper}>
-        <div className={classes.switch}>
-          <label className={classes.pureMaterialSwitch}>
-            <input
-              checked={props.checked}
-              disabled={props.disabled}
-              type='checkbox'
-              onChange={(e) => props.onChange(e.target.checked)}
+    <div className={classes.switchWrapper}>
+      <div className={classes.switch}>
+        <label className={classes.switchLabel}>
+          {props.loading ? (
+            <SpinnerCircularFixed
+              size={24}
+              thickness={120}
+              speed={100}
+              color='#704dff'
+              secondaryColor='#e0dcfc'
             />
-
-            <span>{props.switchName}</span>
-          </label>
-        </div>
-        <div className={classes.switchDescription}>
-          <span>{props.switchDescription}</span>
-        </div>
+          ) : (
+            <>
+              <input
+                type='checkbox'
+                checked={props.checked || false}
+                disabled={props.disabled}
+                onChange={(e) => props.onChange(e.target.checked)}
+              />
+              <span className={classes.switchSlider}></span>
+            </>
+          )}
+          <span className={classes.switchText}>{props.switchName}</span>
+        </label>
       </div>
-    </>
+      <p className={classes.switchDescription}>{props.switchDescription}</p>
+    </div>
   );
 }

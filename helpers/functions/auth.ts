21bb9c84c1f55633d252b6c75c7d4c782328a880
Ravinou <dev@r4ven.fr>
2025-02-23 15:43:46 +0100

    refactor: ⚡ auth.ts type

--- helpers/functions/auth.ts
+++ helpers/functions/auth.ts
@@ -2,10 +2,10 @@
 //Lib
 import { hash, compare } from 'bcryptjs';
 
-export async function hashPassword(password) {
+export async function hashPassword(password: string) {
   return await hash(password, 12);
 }
 
-export async function verifyPassword(password, hashedPassword) {
+export async function verifyPassword(password: string, hashedPassword: string) {
   return await compare(password, hashedPassword);
 }

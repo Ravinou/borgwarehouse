ae1235955a949a70696d95ba9853b62ccd6ed082
Ravinou <dev@r4ven.fr>
2025-02-23 15:08:48 +0100

    refactor: ⚡ improve type for nexauth jwt

--- types/api/next-auth.types.ts
+++ types/api/next-auth.types.ts
@@ -1,3 +1,25 @@
+import { DefaultSession, DefaultUser } from 'next-auth';
+
+declare module 'next-auth' {
+  // Add custom properties to the User object in the session
+  interface User extends DefaultUser {
+    roles?: string[];
+    id?: string;
+  }
+
+  interface Session {
+    user: {
+      roles?: string[];
+      id?: string;
+    } & DefaultSession['user'];
+  }
+
+  interface JWT {
+    roles?: string[];
+    id?: string;
+  }
+}
+
 export enum SessionStatus {
   AUTHENTICATED = 'authenticated',
   UNAUTHENTICATED = 'unauthenticated',

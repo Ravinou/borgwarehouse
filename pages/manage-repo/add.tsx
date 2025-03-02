7ed478c928a13189871a47650c6b9a8ca4fd0c78
Ravinou <dev@r4ven.fr>
2025-03-02 14:04:44 +0100

    refactor: ⚡ version API and some pages index

--- pages/manage-repo/add.tsx
+++ pages/manage-repo/add.tsx
@@ -1,12 +1,13 @@
-import RepoList from '../../../Containers/RepoList/RepoList';
-import { authOptions } from '../../../pages/api/auth/[...nextauth]';
+import { GetServerSidePropsContext } from 'next';
+import RepoList from '~/Containers/RepoList/RepoList';
+import { authOptions } from '../api/auth/[...nextauth]';
 import { getServerSession } from 'next-auth/next';
 
 export default function Add() {
   return <RepoList />;
 }
 
-export async function getServerSideProps(context) {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
   //Var
   const session = await getServerSession(context.req, context.res, authOptions);
 

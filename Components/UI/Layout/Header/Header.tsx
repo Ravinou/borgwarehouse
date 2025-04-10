5676aab70b53a97d6e211aa16f41340e55be9a8f
Ravinou <dev@r4ven.fr>
2025-04-10 13:16:54 +0200

    ui: 🎨 new borgwarehouse logo and favicon

--- Components/UI/Layout/Header/Header.tsx
+++ Components/UI/Layout/Header/Header.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import classes from './Header.module.css';
 import Nav from './Nav/Nav';
 
@@ -5,7 +6,16 @@ function Header() {
   return (
     <header className={classes.Header}>
       <div className={[classes.flex, 'container'].join(' ')}>
-        <div className={classes.logo}>BorgWarehouse</div>
+        <div className={classes.logo}>
+          <Image
+            src='/borgwarehouse-logo-violet.svg'
+            alt='BorgWarehouse'
+            width={225}
+            height={40}
+            className={classes.logoImage}
+            priority
+          />
+        </div>
 
         <nav>
           <Nav />

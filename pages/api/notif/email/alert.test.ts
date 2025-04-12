0ee771f64ad9faa6e16695faef7f93314bfb0c84
Ravinou <dev@r4ven.fr>
2025-04-12 19:20:30 +0200

    refactor: ⚡ email API rest compliant

--- pages/api/notif/email/alert.test.ts
+++ pages/api/notif/email/alert.test.ts
@@ -1,12 +1,104 @@
-import { createMocks } from 'node-mocks-http';
-import handler from '~/pages/api/account/updateEmailAlert';
 import { getServerSession } from 'next-auth/next';
+import { createMocks } from 'node-mocks-http';
+import handler from '~/pages/api/notif/email/alert';
 import { ConfigService } from '~/services';
 
 vi.mock('next-auth/next');
 vi.mock('~/services');
 
-describe('PUT /api/account/updateEmailAlert', () => {
+describe('Get Email Alert API', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+    vi.spyOn(console, 'log').mockImplementation(() => {});
+  });
+
+  it('should return 405 if the method is not GET', async () => {
+    vi.mocked(getServerSession).mockResolvedValue({
+      user: { name: 'testuser' },
+    });
+    const { req, res } = createMocks({ method: 'POST' });
+    await handler(req, res);
+    expect(res._getStatusCode()).toBe(405);
+  });
+
+  it('should return 401 if the user is not authenticated', async () => {
+    vi.mocked(getServerSession).mockResolvedValue(null);
+    const { req, res } = createMocks({ method: 'GET' });
+    await handler(req, res);
+    expect(res._getStatusCode()).toBe(401);
+  });
+
+  it('should return 400 if the user does not exist', async () => {
+    vi.mocked(getServerSession).mockResolvedValue({
+      user: { name: 'nonexistent' },
+    });
+
+    vi.mocked(ConfigService.getUsersList).mockResolvedValue([
+      {
+        id: 1,
+        username: 'testuser',
+        email: 'testuser@example.com',
+        password: 'hashedpassword',
+        roles: ['user'],
+        emailAlert: true,
+      },
+    ]);
+
+    const { req, res } = createMocks({ method: 'GET' });
+    await handler(req, res);
+
+    expect(res._getStatusCode()).toBe(400);
+    expect(res._getJSONData()).toEqual({
+      message: 'User is incorrect. Please, logout to update your session.',
+    });
+  });
+
+  it('should return emailAlert if the user exists', async () => {
+    vi.mocked(getServerSession).mockResolvedValue({
+      user: { name: 'testuser' },
+    });
+
+    vi.mocked(ConfigService.getUsersList).mockResolvedValue([
+      {
+        id: 1,
+        username: 'testuser',
+        email: 'testuser@example.com',
+        password: 'hashedpassword',
+        roles: ['user'],
+        emailAlert: true,
+      },
+    ]);
+
+    const { req, res } = createMocks({ method: 'GET' });
+    await handler(req, res);
+
+    expect(res._getStatusCode()).toBe(200);
+    expect(res._getJSONData()).toEqual({
+      emailAlert: true,
+    });
+  });
+
+  it('should return 500 if there is an error reading the file', async () => {
+    vi.mocked(getServerSession).mockResolvedValue({
+      user: { name: 'testuser' },
+    });
+
+    vi.mocked(ConfigService.getUsersList).mockImplementation(() => {
+      throw new Error();
+    });
+
+    const { req, res } = createMocks({ method: 'GET' });
+    await handler(req, res);
+
+    expect(res._getStatusCode()).toBe(500);
+    expect(res._getJSONData()).toEqual({
+      status: 500,
+      message: 'API error, contact the administrator',
+    });
+  });
+});
+
+describe('Update email Alert API', () => {
   beforeEach(() => {
     vi.clearAllMocks();
     vi.spyOn(console, 'log').mockImplementation(() => {});
@@ -22,6 +114,9 @@ describe('PUT /api/account/updateEmailAlert', () => {
   });
 
   it('should return 405 if method is not PUT', async () => {
+    vi.mocked(getServerSession).mockResolvedValue({
+      user: { name: 'testuser' },
+    });
     const { req, res } = createMocks({ method: 'POST' });
     await handler(req, res);
 

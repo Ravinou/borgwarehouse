0d45f4fe7faa5f72086acb99f9fc4f2d570b026e
Ravinou <dev@r4ven.fr>
2026-06-21 20:00:12 +0200

    ui: 🎨 better quick command display

--- Components/Repo/QuickCommands/QuickCommands.tsx
+++ Components/Repo/QuickCommands/QuickCommands.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import { useState } from 'react';
 import classes from './QuickCommands.module.css';
-import { IconSettingsAutomation, IconCopy } from '@tabler/icons-react';
+import { IconTerminal2, IconCopy, IconCheck } from '@tabler/icons-react';
 import { lanCommandOption } from '~/helpers/functions';
 import { WizardEnvType } from '~/types/domain/config.types';
 
@@ -16,47 +15,47 @@ export default function QuickCommands(props: QuickCommandsProps) {
   //Needed to generate command for borg over LAN instead of WAN if env vars are set and option enabled.
   const { FQDN, SSH_SERVER_PORT } = lanCommandOption(wizardEnv, props.lanCommand);
 
+  const connectionString = `ssh://${wizardEnv?.UNIX_USER}@${FQDN}${
+    SSH_SERVER_PORT ? SSH_SERVER_PORT : ''
+  }/./${props.repositoryName}`;
+
   const [isCopied, setIsCopied] = useState(false);
 
   const handleCopy = async () => {
-    // Asynchronously call copy to clipboard
     navigator.clipboard
-      .writeText(
-        `ssh://${wizardEnv?.UNIX_USER}@${FQDN}${SSH_SERVER_PORT ? SSH_SERVER_PORT : ''}/./${props.repositoryName}`
-      )
+      .writeText(connectionString)
       .then(() => {
         setIsCopied(true);
-        setTimeout(() => {
-          setIsCopied(false);
-        }, 1500);
+        setTimeout(() => setIsCopied(false), 1500);
       })
-      .catch((err) => {
-        console.log(err);
-      });
+      .catch((err) => console.log(err));
   };
 
   return (
-    <div className={classes.container}>
-      {isCopied ? (
-        <div className={classes.copyValid}>Copied !</div>
-      ) : (
-        <div className={classes.tooltip}>
-          ssh://{wizardEnv?.UNIX_USER}@{FQDN}
-          {SSH_SERVER_PORT ? SSH_SERVER_PORT : ''}/./
-          {props.repositoryName}
-        </div>
-      )}
-
-      {props.lanCommand && <div className={classes.lanBadge}>LAN</div>}
-
-      <div className={classes.icons}>
-        <button onClick={handleCopy} className={classes.copyButton}>
-          <IconCopy color='#65748b' stroke={1.25} />
-        </button>
-        <div className={classes.quickSetting}>
-          <IconSettingsAutomation color='#65748b' stroke={1.25} />
-        </div>
-      </div>
+    <div className={classes.commandBox}>
+      <IconTerminal2 size={15} className={classes.commandIcon} stroke={1.75} />
+      <code className={classes.commandText} title={connectionString}>
+        {connectionString}
+      </code>
+      {props.lanCommand && <span className={classes.lanBadge}>LAN</span>}
+      <button
+        onClick={handleCopy}
+        className={`${classes.copyButton} ${isCopied ? classes.copied : ''}`}
+        title='Copy connection string'
+        aria-label='Copy connection string'
+      >
+        {isCopied ? (
+          <>
+            <IconCheck size={13} stroke={2} />
+            Copied
+          </>
+        ) : (
+          <>
+            <IconCopy size={13} stroke={1.75} />
+            Copy
+          </>
+        )}
+      </button>
     </div>
   );
 }

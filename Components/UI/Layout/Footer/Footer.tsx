c6d2a6c8f15c2bd512d203155796bbcade443d44
Ravinou <dev@r4ven.fr>
2026-06-22 20:09:08 +0200

    ui: 🎨 improve footer

--- Components/UI/Layout/Footer/Footer.tsx
+++ Components/UI/Layout/Footer/Footer.tsx
@@ -1,22 +1,45 @@
 import classes from './Footer.module.css';
 import packageInfo from '~/package.json';
+import { IconBrandGithub, IconWorld } from '@tabler/icons-react';
 
 function Footer() {
   return (
-    <div className={classes.footer}>
-      <p>
-        About{' '}
+    <footer className={classes.footer}>
+      <div className={classes.inner}>
         <a
-          className={classes.site}
+          className={classes.brand}
           target='_blank'
           href='https://borgwarehouse.com/'
           rel='noreferrer'
         >
           BorgWarehouse
-        </a>{' '}
-        - v{packageInfo.version}
-      </p>
-    </div>
+        </a>
+        <span className={classes.version}>v{packageInfo.version}</span>
+        <span className={classes.sep} aria-hidden='true' />
+        <nav className={classes.links}>
+          <a
+            className={classes.iconLink}
+            href='https://github.com/Ravinou/borgwarehouse'
+            target='_blank'
+            rel='noreferrer'
+            aria-label='GitHub repository'
+            title='GitHub'
+          >
+            <IconBrandGithub size={16} stroke={1.75} />
+          </a>
+          <a
+            className={classes.iconLink}
+            href='https://borgwarehouse.com/'
+            target='_blank'
+            rel='noreferrer'
+            aria-label='Website'
+            title='Website'
+          >
+            <IconWorld size={16} stroke={1.75} />
+          </a>
+        </nav>
+      </div>
+    </footer>
   );
 }
 
